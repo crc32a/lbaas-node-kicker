4dbf41ea4fcb0776f476fef6de5c6001265f64a4
Phillip Toohill <phillip.toohill@rackspace.com>
2011-09-20 17:55:40 -0500

    decomposing buildLbRequests

--- servers/buildLBRequests.py
+++ servers/buildLBRequests.py
@@ -12,14 +12,27 @@ def usage(prog):
      printf("usage is %s <json_config_file>\n",prog)
      printf("\n")
 
-def render_json(json_reqs):
-    jr = util.load_json(file_reqs)
-    return jr
+def render_json_objects(reqs_file):
+    reqs = util.load_json(reqs_file)
+    return reqs
 
 def load_url(file_name):
-    cf = util.load_json(file_name)
-    url = cf["hostUrl"]
+    url = util.load_json(config_file)["url"]
     return url
+
+def load_headers():
+    h = pickle.load(open("auth_headers.db","r"))
+    intended_keys = ["x-auth-token"]
+    headers = {}
+    headers["content-type"] = "application/xml"
+    headers["accept"] = "application/xml"
+    if os.path.isfile("extra_headers.json"):
+       extra_headers = util.load_json("extra_headers.json")
+       headers.update(extra_headers)
+    for auth_key in intended_keys:
+        headers[auth_key]=h[auth_key]
+    printf("\n\nheaders=%s\n",headers)
+    return headers
 
 def write_log(logStr, error, lb, code):
     fp = open("metrics.log", "a")
@@ -34,17 +47,8 @@ def write_log(logStr, error, lb, code):
 
 def build_lbs(reqs,url):    
     for lb in reqs:
-        h = pickle.load(open("auth_headers.db","r"))
-        intended_keys = ["x-auth-token"]
-        headers = {}
-        headers["content-type"] = "application/xml"
-        headers["accept"] = "application/xml"
-        if os.path.isfile("extra_headers.json"):
-            extra_headers = util.load_json("extra_headers.json")
-            headers.update(extra_headers)
-        for auth_key in intended_keys:
-            headers[auth_key]=h[auth_key]
-        printf("\n\nurl=%s\ndata=%s\nheaders=%s\n",url,lb,headers)
+        headers = load_headers()
+        printf("\n\nurl=%s\ndata=%s\n",url,lb)
         request = urllib2.Request(url, lb, headers) 
         try:
             start = time.time()
@@ -60,7 +64,6 @@ def build_lbs(reqs,url):
             code = e.code
             printf("Error code=%s\nbody=%s\n",code,msg)
             write_log(0.0, msg, lb, code)
-            printf("Exception resp.code=%s\n%s\n",e.code,e.read())
 
 if __name__ == "__main__":
     prog = os.path.basename(sys.argv[0])
@@ -68,6 +71,6 @@ if __name__ == "__main__":
         usage(prog)
         sys.exit()
     config_file = sys.argv[1]
-    url = util.load_json(config_file)["url"]
-    lbs = util.load_json("requests.json")
-    build_lbs(lbs,url)
+    url = load_url(config_file)
+    request_objects = render_json_objects("requests.json")
+    build_lbs(request_objects,url)
